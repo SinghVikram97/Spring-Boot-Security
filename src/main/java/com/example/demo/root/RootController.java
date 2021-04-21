7e73bae20ce81f9b53ec6a54c73ec13f5483efb2
Vikram <vbedi@expediagroup.com>
2021-04-21 17:11:40 +0530

    Whitelisted root endpoint

---
+++ src/main/java/com/example/demo/root/RootController.java
@@ -0,0 +1,14 @@
+package com.example.demo.root;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class RootController {
+    @GetMapping("")
+    public String rootPath(){
+        return "Hello Spring Web Security";
+    }
+}
